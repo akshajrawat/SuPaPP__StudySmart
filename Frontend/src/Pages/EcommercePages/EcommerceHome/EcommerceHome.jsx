27d153ab60ee6b45d1f7182e3c76763b8f646047
Akshaj Rawat <akshajr11@gmail.com>
2025-06-20 17:42:08 +0530

    UI fixes

--- Frontend/src/Pages/EcommercePages/EcommerceHome/EcommerceHome.jsx
+++ Frontend/src/Pages/EcommercePages/EcommerceHome/EcommerceHome.jsx
@@ -3,34 +3,44 @@ import { FaSearch } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "../../../Store/Slice/shopSlice";
 import CategoryCarousel from "./Comp/CategoryCarousel";
+import EcomCard from "./Comp/EcomCard";
 
 const EcommerceHome = () => {
   const dispatch = useDispatch();
   const shop = useSelector((state) => state.shop);
+  const products = shop.products;
   // fetching products from backend
   useEffect(() => {
     dispatch(fetchProducts());
   }, []);
 
+  // category selection
   const uniqueCategoryProducts = useMemo(() => {
     const result = [];
     const seen = new Set();
-    for (const product of shop.products) {
+    for (const product of products) {
       if (!seen.has(product.category)) {
         result.push(product);
         seen.add(product.category);
       }
     }
     return result;
-  }, [shop.products]);
+  }, [products]);
 
+  // highest rated
   const topRatedProducts = useMemo(() => {
-    const top = shop.products.filter((product) => product.rating > 4);
+    const top = products.filter((product) => product.rating > 4.8);
     return top;
-  }, [shop.products]);
+  }, [products]);
+
+  // highest Discount
+  const highDiscountProducts = useMemo(() => {
+    const top = products.filter((product) => product.discountPercentage > 17);
+    return top;
+  }, [products]);
 
   return (
-    <div className=" max-w-full h-[calc(100vh-64.8px)] flex flex-col gap-6 p-3">
+    <div className=" max-w-full h-[calc(100vh-64.8px)] flex flex-col gap-6 p-3 overflow-scroll">
       {/* search box  */}
       <div className="flex items-center justify-center h-[8%]">
         <div className="flex items-center w-full bg-gray-100 dark:bg-[#19173a] rounded-full px-4 py-2 shadow-inner">
@@ -60,8 +70,32 @@ const EcommerceHome = () => {
         </div>
 
         {/* top selling showcase */}
-        <div className="bg-red-500 h-[90%] w-full">
-          
+        <div className=" h-[90%] flex gap-3 overflow-x-scroll justify-start ">
+          {topRatedProducts.map((item) => {
+            return (
+              <div key={item.id} className="shrink-0">
+                {" "}
+                <EcomCard item={item}  />{" "}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      {/* best deals */}
+      <div className="h-[50%] flex flex-col gap-3 ">
+        <div className="flex justify-between items-center h-[10%]">
+          <h2 className="text-white text-xl font-semibold "> Best Deals </h2>
+        </div>
+
+        {/* top selling showcase */}
+        <div className=" h-[90%] flex gap-3 overflow-x-scroll justify-start ">
+          {highDiscountProducts.map((item) => {
+            return <div key={item.id} className="shrink-0">
+                {" "}
+                <EcomCard item={item}  />{" "}
+              </div>
+          })}
         </div>
       </div>
     </div>
