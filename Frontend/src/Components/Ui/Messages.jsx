6d6bf24c1ef68ed042dcd5cf054c4a17dc93526d
Akshaj Rawat <akshajr11@gmail.com>
2025-09-15 10:47:29 +0530

    Completely cleaned folder

--- Frontend/src/Components/Ui/Messages.jsx
+++
@@ -1,9 +0,0 @@
-const LoadingSpinner = () => {
-  return (
-    <div className="flex justify-center mt-4">
-      <div className="w-8 h-8 border-4 border-[#4fd1d9] border-t-transparent rounded-full animate-spin"></div>
-    </div>
-  );
-};
-
-export { LoadingSpinner };
